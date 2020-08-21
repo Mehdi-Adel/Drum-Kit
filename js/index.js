6977d50ffeaab4fbe06160d0c9f228b7e8e2cd72
Mehdi-Adel <zeadel.mehdi@gmail.com>
2020-08-21 17:00:10 +0200

    Updates

---
+++ js/index.js
@@ -0,0 +1,74 @@
+// function playSound() {
+//     var audio = new Audio('sounds/tom-1.mp3');
+//     audio.play();
+// }
+
+// Detecting clicks
+
+var drumButtons = document.querySelectorAll("button.drum")
+
+for (i = 0; i < drumButtons.length; i++) {
+    drumButtons[i].addEventListener("click", function () {
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);    
+    });
+}
+
+// Detecting keyboard
+
+document.addEventListener("keydown", function(event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+})
+
+function makeSound(keyboard) {
+    switch (keyboard) {
+        case "a":
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+
+        case "z":
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+
+        case "e":
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+
+        case "r":
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+
+        case "t":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+
+        case "y":
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+
+        case "u":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+
+        default: console.log(keyboard);
+            
+    }
+}
+
+function buttonAnimation(currentKey) {
+    
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100)
+}
